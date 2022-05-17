a9ba710d4fce6c885db72b8715b01eeb9d245463
Aaron-Ekal <ekalaaronkalale@gmail.com>
2022-05-18 01:09:58 +0300

    Folder structure restructured

--- manage.py
+++ manage.py
@@ -3,12 +3,16 @@ from flask import Flask, render_template, url_for
 #create a Flask Instance
 app = Flask(__name__)
 
-
 #Create a route directory
 @app.route('/')
 def home():
     title = "Router Man"
-    return render_template('home.html')
+    return render_template('index.html', title=title)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 
 
 if __name__== '__main__':
