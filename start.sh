4c43948fe2e61052ac8b898d2ff9a9d95615424d
Aaron-Ekal <ekalaaronkalale@gmail.com>
2022-05-19 15:33:23 +0300

    password change instart.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://tellem:1234@localhost/team4'
+export SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://tellem:mko9@localhost/team4'
 export SECRET_KEY = '1234qwertyui'
 
 python3.8 manage.py server
